f81ee00c1f34f307dd7f2172f8893ddfee2f543b
Pat Kaehuaea <>
2020-06-14 16:47:48 -0700

    Fixed over indentation.

--- rc.lua
+++ rc.lua
@@ -15,21 +15,21 @@ local beautiful = require('beautiful')
 require('awful.autofocus')
 
 --  ========================================
--- 			       Theme
---	     	Load the Aesthetics
+--             Theme
+--        Load the Aesthetics
 --  ========================================
 beautiful.init(require('theme'))
 
 --  ========================================
--- 			  	  Layouts
---	     	   Load the Panels
+--            Layouts
+--           Load the Panels
 --  ========================================
 
 require('layout')
 
 --  ========================================
--- 			      Modules
---	        Load all the modules
+--            Modules
+--          Load all the modules
 --  ========================================
 
 require('module.notifications')
@@ -45,8 +45,8 @@ require('module.brightness-osd')
 --require('module.lockscreen')
 
 --  ========================================
--- 				Configuration
---	     	Load your prefrences
+--        Configuration
+--        Load your prefrences
 --  ========================================
 
 require('configuration.client')
@@ -55,11 +55,10 @@ root.keys(require('configuration.keys.global'))
 
 
 --  ========================================
--- 				Hacks :(
---	     	Remove unwanted layouts from default
+--        Hacks :(
+--        Remove unwanted layouts from default
 --  ========================================
 
---awful.layout.remove_default_layout(awful.layout.suit.floating)
 awful.layout.remove_default_layout(awful.layout.suit.tile.left)
 awful.layout.remove_default_layout(awful.layout.suit.tile.bottom)
 awful.layout.remove_default_layout(awful.layout.suit.tile.top)
@@ -79,62 +78,62 @@ lain.layout.termfair.center.nmaster = 3
 lain.layout.termfair.center.ncol    = 1
 
 --  ========================================
--- 					Signals
---	     Signals Listeners and Senders
+--          Signals
+--       Signals Listeners and Senders
 --  ========================================
 
 screen.connect_signal("request::wallpaper", function(s)
 
-	-- If wallpaper is a function, call it with the screen
-	if beautiful.wallpaper then
-		if type(beautiful.wallpaper) == "string" then
-			if beautiful.wallpaper:sub(1, #"#") == "#" then
-					gears.wallpaper.set(beautiful.wallpaper)
-			elseif beautiful.wallpaper:sub(1, #"/") == "/" then
-					gears.wallpaper.maximized(beautiful.wallpaper, s)
-			end
-		else
-			beautiful.wallpaper(s)
-		end
-	end
+  -- If wallpaper is a function, call it with the screen
+  if beautiful.wallpaper then
+    if type(beautiful.wallpaper) == "string" then
+      if beautiful.wallpaper:sub(1, #"#") == "#" then
+          gears.wallpaper.set(beautiful.wallpaper)
+      elseif beautiful.wallpaper:sub(1, #"/") == "/" then
+          gears.wallpaper.maximized(beautiful.wallpaper, s)
+      end
+    else
+      beautiful.wallpaper(s)
+    end
+  end
 
 end)
 
 -- Signal function to execute when a new client appears.
 client.connect_signal(
-	'manage',
-	function(c)
-		-- Set the windows at the slave,
-		-- i.e. put it at the end of others instead of setting it master.
-		if not _G.awesome.startup then
-			awful.client.setslave(c)
-		end
+  'manage',
+  function(c)
+    -- Set the windows at the slave,
+    -- i.e. put it at the end of others instead of setting it master.
+    if not _G.awesome.startup then
+      awful.client.setslave(c)
+    end
 
-		if _G.awesome.startup and not c.size_hints.user_position and not c.size_hints.program_position then
-			-- Prevent clients from being unreachable after screen count changes.
-			awful.placement.no_offscreen(c)
-		end
-	end
+    if _G.awesome.startup and not c.size_hints.user_position and not c.size_hints.program_position then
+      -- Prevent clients from being unreachable after screen count changes.
+      awful.placement.no_offscreen(c)
+    end
+  end
 )
 
 -- Enable sloppy focus, so that focus follows mouse.
 client.connect_signal(
-	'mouse::enter',
-	function(c)
-		c:emit_signal('request::activate', 'mouse_enter', {raise = true})
-	end
+  'mouse::enter',
+  function(c)
+    c:emit_signal('request::activate', 'mouse_enter', {raise = true})
+  end
 )
 
 client.connect_signal(
-	'focus',
-	function(c)
-		c.border_color = beautiful.border_focus
-	end
+  'focus',
+  function(c)
+    c.border_color = beautiful.border_focus
+  end
 )
 
 client.connect_signal(
-	'unfocus',
-	function(c)
-		c.border_color = beautiful.border_normal
-	end
+  'unfocus',
+  function(c)
+    c.border_color = beautiful.border_normal
+  end
 )
