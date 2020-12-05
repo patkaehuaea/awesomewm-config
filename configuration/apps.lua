4fdf101ff5569d1effbbf643832e92faeccc344e
Pat Kaehuaea <>
2020-12-05 10:12:30 -0800

    File had spacing issues and unused applications. Removed and cleaned up spacing.

--- configuration/apps.lua
+++ configuration/apps.lua
@@ -5,21 +5,19 @@ local config_dir = filesystem.get_configuration_dir()
 return {
 	-- The default applications in keybindings and widgets
 	default = {
-		terminal 										= 'alacritty',                                  -- Terminal Emulator
-		text_editor 									= 'vim',                                  -- GUI Text Editor
-		web_browser 									= 'firefox',                                -- Web browser
-		file_manager 									= 'ranger',                                -- GUI File manager
-		network_manager 								= 'networkmanager_dmenu',					-- Network manager
-		bluetooth_manager 								= 'blueman-manager',						-- Bluetooth manager
+		terminal = 'alacritty',                                  -- Terminal Emulator
+		text_editor = 'vim',                                  -- GUI Text Editor
+		web_browser = 'firefox',                                -- Web browser
+		file_manager = 'ranger',                                -- GUI File manager
+		network_manager = 'networkmanager_dmenu',					-- Network manager
+		bluetooth_manager = 'blueman-manager',						-- Bluetooth manager
 		lock = 'xflock4',  -- Lockscreen
-		package_manager 								= 'pamac-manager',							-- GUI Package manager
-		rofiweb											= 'rofi -dpi ' .. screen.primary.dpi .. 
-														  ' -show Search -modi Search:' .. config_dir .. 
-														  '/configuration/rofi/sidebar/rofi-web-search.py' .. 
-														  ' -theme ' .. config_dir ..
-														  '/configuration/rofi/sidebar/rofi.rasi', 	-- Rofi Web Search
-
-		rofiappmenu 									= 'rofi -dpi ' .. screen.primary.dpi ..
-														  ' -show drun -theme ~/.config/rofi/launchers/nord.rasi'   -- Application Menu
+		package_manager = 'pamac-manager',							-- GUI Package manager
+		rofiappmenu = 
+		  'rofi -dpi ' 
+		  .. screen.primary.dpi 
+		  .. ' -show drun -theme ' 
+		  .. config_dir 
+		  .. 'configuration/rofi/appmenu/nord.rasi'
 	},
 }
